8db3c2ba2b30368d74efd723e1c291df1ea07eb9
Chaitanya Gupta <chaitanya.gupta@veersatech.com>
2024-12-19 15:52:17 +0530

    Updates

--- src/events/base-publisher.ts
+++ src/events/base-publisher.ts
@@ -17,6 +17,7 @@ export abstract class Publisher<T extends Event> {
   publish(data: T["data"]): Promise<void> {
     return new Promise((resolve, reject) => {
       this.client.publish(this.subject, JSON.stringify(data));
+      console.log("Event published to subject", this.subject);
     });
   }
 }
